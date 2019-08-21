93c07bd3a015b729001768ed960e011adfd17171
azimkhan <wanrossen@gmail.com>
2019-08-22 00:09:04 +0600

    initial

---
+++ main.go
@@ -0,0 +1,57 @@
+package main
+
+import (
+	"context"
+	"encoding/json"
+	"github.com/heetch/confita"
+	"github.com/heetch/confita/backend/env"
+	"github.com/heetch/confita/backend/file"
+	"go.uber.org/zap"
+	"io/ioutil"
+	"log"
+)
+
+type Config struct {
+	LoggerConfig string `config:"loggerConfig"`
+}
+
+func main() {
+	conf := loadConfig()
+	logger := createLogger(conf.LoggerConfig)
+	logger.Info("logger created successfully")
+	defer logger.Sync()
+}
+
+// Read configuration from file and env variables
+func loadConfig() *Config {
+	loader := confita.NewLoader(
+		env.NewBackend(),
+		file.NewBackend("config.json"),
+	)
+	config := Config{
+		"logger.json",
+	}
+	err := loader.Load(context.Background(), &config)
+	if err != nil {
+		log.Fatal(err)
+	}
+	return &config
+}
+
+// Read logger configuration from file
+func createLogger(configFile string) *zap.Logger {
+	zapConf := zap.Config{}
+	rawJson, e := ioutil.ReadFile(configFile)
+	if e != nil {
+		log.Fatal(e)
+	}
+	e = json.Unmarshal(rawJson, &zapConf)
+	if e != nil {
+		log.Fatal(e)
+	}
+	logger, e := zapConf.Build()
+	if e != nil {
+		log.Fatal(e)
+	}
+	return logger
+}
